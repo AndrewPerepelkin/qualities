362e99d9e9dd8e39c75b4e7b0925835d24c202df
AndrewPerepelkin <avp.tep@rambler.ru>
2022-11-24 02:29:08 +0300

    feat: added sentry errors logger

--- src/app/services/httpServices.js
+++ src/app/services/httpServices.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+import logger from "./logServices";
 
 axios.interceptors.response.use(res => res, function (error) {
     const expectedErrors = error.response && error.response.status >= 400 && error.response.status < 500;
     if (!expectedErrors) {
+        logger.log(error);
         console.log("Unexpected error");
     }
     return Promise.reject(error);
---
+++ src/app/services/logServices.js
@@ -0,0 +1,25 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn: "https://3d695cd028464b82a9b87e23ed5b6037@o4504210819842048.ingest.sentry.io/4504210828361728",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+const logger = {
+  init,
+  log,
+}
+
+export default logger;
